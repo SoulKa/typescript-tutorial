01f2f5e43dd39791d286cb786e2cff0e5e6d8814
Wilke, Louis <louis.wilke@exxeta.com>
2023-10-28 14:15:26 +0200

    add comments to solution 1

--- solutions/excercise01/index.ts
+++ solutions/excercise01/index.ts
@@ -1,4 +1,6 @@
 import {getRandomName} from "./src/random-name-generator";
 
 const name = getRandomName();
-console.log(`Hello ${name}`);
+console.log(`Hello ${name}`); // Template string
+console.log("Hello " + name); // String concatenation
+console.log(["Hello", name].join("")); // Array join
--- solutions/excercise01/src/random-name-generator.ts
+++ solutions/excercise01/src/random-name-generator.ts
@@ -1,6 +1,10 @@
 const NAMES = ["Alex", "Şaner", "Michael", "Amir", "Birte", "Delano", "Daniel", "Raphael", "Philipp", "Manuel", "Vera", "Sven", "Vlad", "Louis"];
 
+/**
+ * Returns a random name.
+ */
 export function getRandomName(): string {
+  // Math.random() returns a random floating point number between 0 (including) and 1 (excluding).
   const index = Math.floor(Math.random() * NAMES.length);
   return NAMES[index];
 }
