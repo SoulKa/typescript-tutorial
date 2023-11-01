325569c620e27552b9f102cf47bf3a7ca9d2bd81
Wilke, Louis <louis.wilke@exxeta.com>
2023-11-01 18:39:45 +0100

    improve http error class

--- solutions/exercise03/src/http-client/http-error.ts
+++ solutions/exercise03/src/http-client/http-error.ts
@@ -1,13 +1,11 @@
 export class HttpError extends Error {
-
-  public readonly status: number;
-
+  
   /**
    * Creates a new HttpError
    * @param url The URL that was called
    * @param status The status code of the response
    */
-  constructor(url: string, status: number) {
+  constructor(url: string, public readonly status: number) {
     super(`The HTTP call to ${url} responded with status code ${status}`);
     this.status = status;
   }
