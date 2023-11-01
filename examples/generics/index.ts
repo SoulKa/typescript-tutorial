44c5bd60f3fc28bf1adeffc8224c847ca3e35a57
Wilke, Louis <louis.wilke@exxeta.com>
2023-11-01 09:06:16 +0100

    modify generic example

--- examples/generics/index.ts
+++ examples/generics/index.ts
@@ -1,12 +1,12 @@
-function parseJson<T>(json: string) {
+function parseJson<T extends object>(json: string) {
   return JSON.parse(json) as T;
 }
 
-class JsonParser<T> {
+class JsonParser<T extends (object | null) = object> {
   parse(json: string) {
     return JSON.parse(json) as T;
   }
 }
 
 parseJson<{ test: number }>(`{"test":123}`);
-new JsonParser<{ test: number }>().parse(`{"test":123}`);
+new JsonParser<Record<string, number>>().parse(`{"test":123}`);
